5a98fa2c2f1822b6a31abb17ca272f3e256a0b48
SamuraiT <t.yasukawa01@gmail.com>
2014-07-04 17:59:32 +0900

    add setup info

--- setup.py
+++ setup.py
@@ -1,8 +1,24 @@
-"""
-Copyright 2014 Tatsuro Yasukawa.
+from distutils.core import setup
 
-Distributed under the GNU General Public License at
-gnu.org/licenses/gpl.html.
-"""
-
+setup(
+    name = 'tinysegmenter',
+    packages = ['tinysegmenter'],
+    version = '0.0.2',
+    description = 'Super compact Japanese tokenizer',
+    maintainer = 'Tatsuro Yasukawa',
+    maintainer_email = 't.yasukawa01@gmail.com',
+    url = 'https://github.com/SamuraiT/tinysegmenter',
+    license='BSD',
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "System :: OS Independent Development Status :: 3 - Alpha",
+        "Environment :: Other",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Text Processing :: Linguistic",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
 
