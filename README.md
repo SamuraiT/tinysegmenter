53fc589d7305634f2c5862d6ad5ea3a13fa47900
SamuraiT <t.yasukawa01@gmail.com>
2014-07-14 15:47:48 +0900

    add long description

--- README.md
+++ README.md
@@ -2,13 +2,13 @@ TinySegmenter
 ----------
 
 TinySegmenter -- Super compact Japanese tokenizer was originally created by 
-(c) 2008 Taku Kudo <taku@chasen.org> for javascript under the terms of a new BSD licence.
+(c) 2008 Taku Kudo for javascript under the terms of a new BSD licence.
 For details, see [here](http://lilyx.net/pages/tinysegmenter_licence.txt)
 
 tinysegmenter for python2.x was written by Masato Hagiwara.
 for his information see [here](http://lilyx.net/pages/tinysegmenterp.html)
 
-This tinysegmenter is modified a bit for python3.x and for distribution by Tatsuro Yasukawa.
+This tinysegmenter is modified for python3.x and python2.x for distribution by Tatsuro Yasukawa.
 
 See info about [tinysegmenter](https://github.com/SamuraiT/tinysegmenter)
 
--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 from distutils.core import setup
+import os
+
+def read_file(filename):
+    filepath = os.path.join(
+            os.path.dirname(os.path.dirname(__file__)), filename)
+    if os.path.exists(filepath):
+        return open(filepath).read()
+    else:
+        return ''
 
 setup(
     name = 'tinysegmenter3',
     packages = ['tinysegmenter'],
-    version = '0.0.2',
+    version = '0.0.3',
     description = 'Super compact Japanese tokenizer',
     maintainer = 'Tatsuro Yasukawa',
     maintainer_email = 't.yasukawa01@gmail.com',
     url = 'https://github.com/SamuraiT/tinysegmenter',
     license='New BSD',
+    long_description = read_file('README.md'),
     classifiers = [
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
